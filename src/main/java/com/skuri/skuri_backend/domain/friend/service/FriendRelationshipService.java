8b2b7d473c57c3fbbe69160527798696f1ba4e03
jisung-louis <khkh9357@naver.com>
2026-08-23 16:54:03 +0900

    fix: 시간표 공유 예외 정리를 Academic 경계로 이동

--- src/main/java/com/skuri/skuri_backend/domain/friend/service/FriendRelationshipService.java
+++ src/main/java/com/skuri/skuri_backend/domain/friend/service/FriendRelationshipService.java
@@ -2,7 +2,7 @@ package com.skuri.skuri_backend.domain.friend.service;
 
 import com.skuri.skuri_backend.common.exception.BusinessException;
 import com.skuri.skuri_backend.common.exception.ErrorCode;
-import com.skuri.skuri_backend.domain.academic.repository.TimetableShareOverrideRepository;
+import com.skuri.skuri_backend.domain.academic.service.TimetableSharingRelationshipCleanupService;
 import com.skuri.skuri_backend.domain.friend.dto.response.FriendSummaryResponse;
 import com.skuri.skuri_backend.domain.friend.entity.FriendPreference;
 import com.skuri.skuri_backend.domain.friend.entity.FriendRequest;
@@ -36,7 +36,7 @@ public class FriendRelationshipService {
     private final FriendRequestExpiryService friendRequestExpiryService;
     private final FriendSummarySnapshotFactory friendSummarySnapshotFactory;
     private final MemberRepository memberRepository;
-    private final TimetableShareOverrideRepository timetableShareOverrideRepository;
+    private final TimetableSharingRelationshipCleanupService timetableSharingRelationshipCleanupService;
 
     @Transactional
     public FriendRequestCreationResult createRequest(String requesterMemberId, String targetFriendPublicId) {
@@ -124,8 +124,7 @@ public class FriendRelationshipService {
         friendshipRepository.delete(friendship);
         friendPreferenceRepository.deleteByOwnerMemberIdAndFriendMemberId(ownerMemberId, friendMemberId);
         friendPreferenceRepository.deleteByOwnerMemberIdAndFriendMemberId(friendMemberId, ownerMemberId);
-        timetableShareOverrideRepository.deleteByOwnerMemberIdAndFriendMemberId(ownerMemberId, friendMemberId);
-        timetableShareOverrideRepository.deleteByOwnerMemberIdAndFriendMemberId(friendMemberId, ownerMemberId);
+        timetableSharingRelationshipCleanupService.deleteOverridesForMemberPair(ownerMemberId, friendMemberId);
     }
 
     @Transactional
@@ -150,8 +149,7 @@ public class FriendRelationshipService {
         friendshipRepository.findByMemberPairForUpdate(pair.lowMemberId(), pair.highMemberId()).ifPresent(friendshipRepository::delete);
         friendPreferenceRepository.deleteByOwnerMemberIdAndFriendMemberId(blockerMemberId, blockedMemberId);
         friendPreferenceRepository.deleteByOwnerMemberIdAndFriendMemberId(blockedMemberId, blockerMemberId);
-        timetableShareOverrideRepository.deleteByOwnerMemberIdAndFriendMemberId(blockerMemberId, blockedMemberId);
-        timetableShareOverrideRepository.deleteByOwnerMemberIdAndFriendMemberId(blockedMemberId, blockerMemberId);
+        timetableSharingRelationshipCleanupService.deleteOverridesForMemberPair(blockerMemberId, blockedMemberId);
         memberBlockRepository.save(MemberBlock.create(blockerMemberId, blockedMemberId));
     }
 
